dd497d64e29fc57962da6f7bcfb05a18245ca872
Syukron Rizqi Ardhi <syukron.tkj2@gmail.com>
2017-04-12 14:20:42 +0700

    Adding get user by id

--- user.js
+++ user.js
@@ -9,8 +9,11 @@ function USER_ROUTER(router,pool) {
 USER_ROUTER.prototype.handleRoutes= function(router,pool) {
     
     router.get("/users/:depot",function(req,res){
-    	var data = {"error":true,
-			    "error_msg":""};
+    	var data = {
+    		error:true,
+			error_msg:"",
+			users : []
+		};
 
 		if (req.params.depot === "Admin_1") {
 			var query = `SELECT kode_spg, nama_spg, nama_toko, depot
@@ -27,12 +30,15 @@ USER_ROUTER.prototype.handleRoutes= function(router,pool) {
 		    connection.query(query,function(err,rows){
 		        connection.release();
 	           	if(err) {
-		            res.json({"error" : true, "error_msg" : "Error executing MySQL query"});
-		        } else {
+		            res.status(500);
+		            data.error_msg = "Error executing MySQL query";
+			        res.json(data);
+			    } else {
 		            if(rows.length > 0){
-		               	data["error"] = false;
-				        data["error_msg"] = 'Success..';
-				        data["users"] = rows;
+		               	data.error = false;
+				        data.error_msg = 'Success..';				        
+				        data.users = rows;
+				        res.status(200);
 				        res.json(data);
 			        }else{
 			            data["error_msg"] = 'No users Found..';
@@ -43,6 +49,42 @@ USER_ROUTER.prototype.handleRoutes= function(router,pool) {
 		    });
 	    });
 	    
+    });
+
+    router.get("/user/:kode_spg",function(req,res){
+    	var data = {
+    		error:true,
+			error_msg:"",
+			user : {}
+		};
+
+		var query = `SELECT kode_spg, nama_spg, nama_toko, depot
+        			FROM t_user WHERE kode_spg = ?`;	
+		
+	    var table = [req.params.kode_spg];
+	    query = mysql.format(query,table);
+	    pool.getConnection(function(err,connection){
+		    connection.query(query,function(err,rows){
+		        connection.release();
+	           	if(err) {
+		            res.status(500);
+		            data.error_msg = "Error executing MySQL query";
+			        res.json(data);
+		        } else {
+		            if(rows.length > 0){
+		               	data.error = false;
+				        data.error_msg = 'Success..';
+				        data.user = rows[0];
+				        res.status(200);
+				        res.json(data);
+			        }else{
+			            data.error_msg = 'No user Found..';
+			            res.status(404);
+			            res.json(data);
+			        }
+		        }
+		    });
+	    });
     });
 }
 
