78c5b2b07c2c9132e517fccf882aa90a4cc09115
syuria27 <syukron.tkj2@gmail.com>
2017-04-20 20:27:50 +0700

    Edit response

--- login.js
+++ login.js
@@ -9,12 +9,16 @@ function LOGIN_ROUTER(router,pool,md5) {
 LOGIN_ROUTER.prototype.handleRoutes= function(router,pool,md5) {
     
     router.post("/login",function(req,res){
-    	var data = {"error":true,
-			    "error_msg":""};
+    	var data = {
+    		error:true,
+			error_msg:"",
+			user : {}
+		};
 
         if (isset(req.body.username) && isset(req.body.password)) {
 			var query = `SELECT username, password,
 						(case when hak_akses = 'Admin_1' then username else nama_spg end) as nama_spg ,
+						(case when hak_akses = 'Admin_1' then 'Nippon Paint' else nama_toko end) as nama_toko ,
 						(case when hak_akses = 'Admin_1' then hak_akses else depot end) as depot FROM t_login l
 	         			LEFT JOIN t_user u ON u.kode_spg = l.username 
 	         			WHERE l.username = ? AND (l.hak_akses = 'Admin' OR l.hak_akses = 'Admin_1')`;
@@ -24,21 +28,27 @@ LOGIN_ROUTER.prototype.handleRoutes= function(router,pool,md5) {
 		        connection.query(query,function(err,rows){
 		            connection.release();
 	            	if(err) {
+						res.status(500);
 		                res.json({"error" : true, "error_msg" : "Error executing MySQL query"});
 		            } else {
 		                if(rows.length != 0){
 		                	if (rows[0].password == md5(req.body.password)) {
-					            data["error"] = false;
-					            data["error_msg"] = 'Success..';
-					            data["user"] = rows[0];
+					            data.error = false;
+					            data.error_msg = 'Success..';
+					            data.user = {
+									kode_spg : rows[0].username,
+									nama_spg : rows[0].nama_spg,
+									nama_toko : rows[0].nama_toko,
+									depot : rows[0].depot
+								};
 					            res.json(data);
 					        }else{
-					        	data["error_msg"] = 'Login fail check password..';
+					        	data.error_msg = 'Login fail check password..';
 					        	res.status(403);
 				            	res.json(data);
 					        }
 				        }else{
-				            data["error_msg"] = 'No users Found..';
+				            data.error_msg = 'No users Found..';
 				            res.status(404);
 				            res.json(data);
 				        }
@@ -46,7 +56,7 @@ LOGIN_ROUTER.prototype.handleRoutes= function(router,pool,md5) {
 		        });
 	    	});
 	    }else{
-	    	data["error_msg"] = 'Missing some params..';
+	    	data.error_msg = 'Missing some params..';
 	    	res.status(400);
 	        res.json(data);
 	    }
